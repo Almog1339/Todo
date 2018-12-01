9fb0a3c13e86e9018ae25bcb05090af297a7cb5d
Almog Amos <33369377+Almog1339@users.noreply.github.com>
2018-12-01 23:34:22 +0200

    ToDo list
    Todo list

---
+++ To-Do lists/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo
+{
+    public class Users
+    {
+        public int user_id { get; set; }
+        public string UserName { get; set; }
+        public string Pass { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Gender { get; set; }
+        public DateTime Date_of_birth { get; set; }
+        public DateTime Created_at { get; set; }
+        public int List_id { get; set; }
+
+    }
+}
